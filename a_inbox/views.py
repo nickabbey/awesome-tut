5b0d4de4c6a92458c13eeca5efcf7181e0c22459
Nick Abbruzzese <nickabbey@gmail.com>
2024-04-29 14:29:49 -0400

    Lesson 22 done

--- a_inbox/views.py
+++ a_inbox/views.py
@@ -13,6 +13,10 @@ def inbox_view(request, conversation_id=None):
 
     if conversation_id:
         conversation = get_object_or_404(my_conversations, id=conversation_id)
+        latest_message = conversation.messages.first()
+        if conversation.is_seen == False and latest_message.sender != request.user:
+            conversation.is_seen = True
+            conversation.save()
     else:
         conversation = None
 
@@ -56,6 +60,7 @@ def new_message(request, recipient_id):
                     message.conversation = c
                     message.save()
                     c.lastmessage_created = timezone.now()
+                    c.is_seen = False
                     c.save()
                     return redirect('inbox', c.id)
             new_conversation = Conversation.objects.create()
@@ -86,6 +91,8 @@ def new_reply(request, conversation_id):
             message.conversation = conversation
             message.save()
             conversation.lastmessage_created = timezone.now()
+            conversation.is_seen = False
+            conversation.save()
             return redirect('inbox', conversation.id)
 
     context = {
@@ -93,3 +100,20 @@ def new_reply(request, conversation_id):
         'conversation': conversation,
     }
     return render(request, 'a_inbox/form_newreply.html', context)
+
+
+def notify_newmessage(request, conversation_id):
+    conversation = get_object_or_404(Conversation, id=conversation_id)
+    latest_message = conversation.messages.first()
+    if conversation.is_seen == False and latest_message.sender != request.user:
+        return render(request, 'a_inbox/notify_icon.html')
+    else:
+        HttpResponse('')
+
+def notify_inbox(request):
+    my_conversations = Conversation.objects.filter(participants=request.user, is_seen=False)
+    for c in my_conversations:
+        latest_message = c.messages.first()
+        if latest_message.sender != request.user:
+            return render(request, 'a_inbox/notify_icon.html')
+    return HttpResponse('')
